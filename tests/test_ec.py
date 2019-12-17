d695b6ef34e5b21a2aa92c25bcaa82f727feb652
Charles Smith <charles@sollum.tech>
2019-12-16 19:41:30 -0500

    Very basic testing.

---
+++ tests/test_ec.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+
+from google.cloud import kms
+
+from gcpjwt.jwt import JWT
+from gcpjwt.jwt_signer import JWTSigner
+
+
+class ECTokenTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        client = kms.KeyManagementServiceClient.from_service_account_file('../resources/google.json')
+        cls.signer = JWTSigner(client, os.environ['PROJECT'], os.environ['LOCATION'])
+
+    def test_basic(self):
+        jwt = JWT(self.__class__.signer, os.environ['RING'], 'jwt-ec')
+        jwt.iss = 'GCPJWT'
+        jwt.aud = 'https://github.com'
+
+        token = jwt.token()
+        self.assertTrue(jwt.verify(token) == 0)
